1f88bd248e4698af6cb7e4c7fdd9efe9add24626
Kai Tillman <kjtillma@valdosta.edu>
2011-09-20 16:40:50 -0400

    Created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ACMApp::Application.config.secret_token = '888ebccd00bd00ca9d224fc0e6a4f143374e1c0149ff3ea6959bd560160d2551d48e9cffe78b195cfbbd9e9e446db7979c0a7216baaa56e7b1ae7e78b0777c7c'
