eb5b696fb3983ec7913a7e89462d022995ec7507
Kai Tillman <kjtillma@valdosta.edu>
2011-09-28 10:39:47 -0400

    Added image for ACM membership and updated seeds.rb to allow membership item to be easily placed in catalog.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Product.delete_all
+Product.create(:title => 'VSU ACM Membership',
+  :description =>
+    %{<p>
+        This item allows you to pay your membership dues online.
+      </p>},
+  :image_url => '/images/acm.jpg',
+  :price => 20.00)
