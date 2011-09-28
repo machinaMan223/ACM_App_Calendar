bb3cf0b9addd129b0033ce459e576a3841cc7e1c
Kai Tillman <kjtillma@valdosta.edu>
2011-09-28 11:21:10 -0400

    Did more work on the cart.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -17,7 +17,7 @@ class CartsController < ApplicationController
       @cart = Cart.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to store_url, :notice => 'Invalid cart'
+      redirect_to store_index_path, :notice => 'Invalid cart'
     else
       respond_to do |format|
         format.html # show.html.erb
@@ -77,11 +77,12 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.xml
   def destroy
-    @cart = Cart.find(params[:id])
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to(carts_url) }
+      format.html { redirect_to(store_index_path, :notice => 'Your cart is currently empty') }
       format.xml  { head :ok }
     end
   end
