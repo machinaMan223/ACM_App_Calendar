7ca4473409f27479e90ee8fd69258928ae714c55
Robert Jenkins <rpjenkins@valdosta.edu>
2011-09-29 11:01:52 -0400

    made changes to event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ActiveRecord::Base
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :duration, :numericality => {:greater_than_or_equal_to => 0}
   validates :name, :uniqueness => true
-  validates :image_url, :format => {
+  validates :icon, :format => {
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
