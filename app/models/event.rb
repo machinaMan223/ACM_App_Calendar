bd55dd6dd1646d1a49ab65abad272282b21d08b8
Robert Jenkins <rpjenkins@valdosta.edu>
2011-09-29 10:58:19 -0400

    made changes to event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,10 +6,6 @@ class Event < ActiveRecord::Base
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :duration, :numericality => {:greater_than_or_equal_to => 0}
   validates :name, :uniqueness => true
-  validates :eventDate, :format => (%m/%d/%Y,
-    :message => 'date must be in MM/DD/YYYY format')
-  validates :eventTime, :format => (%m/%d/%Y,
-    :message => 'time must be in HH:MM format')
   validates :image_url, :format => {
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
