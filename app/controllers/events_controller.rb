b072830ff00d98e55aff29ec5521c7db585d6e82
Kai Tillman <kjtillma@valdosta.edu>
2011-10-12 22:43:30 -0400

    Link to event show from event calendar image.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  skip_before_filter :authorize, :only => :show
+  skip_before_filter :admin_authorize, :only => :show
   # GET /events
   # GET /events.xml
   
