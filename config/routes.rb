c12f828b26204b4c04236f6ebea458302d080cde
Robert Jenkins <rpjenkins@valdosta.edu>
2011-10-17 14:02:41 -0400

    successfull splicing of files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ACMApp::Application.routes.draw do
-  match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
+  match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar,
+  :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
 
   get "event_calendar/index"
 
@@ -8,8 +9,9 @@ ACMApp::Application.routes.draw do
   resources :orders
 
   get 'admin' => 'admin#index'
+  
   controller :sessions do
-get 'login' => :new
+    get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
@@ -25,7 +27,7 @@ get 'login' => :new
   get "store/index"
 
   resources :products do
-get :who_bought, :on => :member
+    get :who_bought, :on => :member
   end
 
   get "acm_app/index"
